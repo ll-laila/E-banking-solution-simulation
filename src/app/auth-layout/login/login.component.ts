f996c8cd962291859a7b24437751bcf7515c7e12
Enami Fatima Zahrae <enamifatimazahrae@gmail.com>
2024-05-20 14:35:30 +0000

    modify navbar

---
+++ src/app/auth-layout/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  standalone: true,
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit() {
+  }
+  ngOnDestroy() {
+  }
+
+}
