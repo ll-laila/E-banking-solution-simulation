d54668abc10a1d6d6cba7d9da8f7c4d40883ea32
Enami Fatima Zahrae <enamifatimazahrae@gmail.com>
2024-05-23 17:02:20 +0000

    modify admin page

--- src/app/Back-office/admin-dash/admin-dash.module.ts
+++ src/app/Back-office/admin-dash/admin-dash.module.ts
@@ -10,6 +10,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {ProfileAdminComponent} from "../pages/profileAdmin/profileAdmin.component";
 import {NewAgentComponent} from "../pages/new-agent/new-agent.component";
 import {AdminDashRoutes} from "./admin-dash.routing";
+import {NgxPaginationModule} from "ngx-pagination";
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import {AdminDashRoutes} from "./admin-dash.routing";
     FormsModule,
     HttpClientModule,
     NgbModule,
-    ClipboardModule
+    ClipboardModule,
+    NgxPaginationModule
   ],
   declarations: [
     ProfileAdminComponent,
