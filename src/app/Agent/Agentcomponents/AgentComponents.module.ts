281b08821469a48067160c045de0b658f5298ec0
Salwahnid <salwahnid1@gmail.com>
2024-05-23 23:38:35 +0100

    agent cRUD client

--- src/app/Agent/Agentcomponents/AgentComponents.module.ts
+++ src/app/Agent/Agentcomponents/AgentComponents.module.ts
@@ -7,6 +7,8 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AddClientComponent } from './add-client/add-client.component';
 import {FormsModule} from '@angular/forms';
+import { EditClientComponent } from './edit-client/edit-client.component';
+import { DetailsClientComponent } from './details-client/details-client.component';
 
 @NgModule({
     imports: [
@@ -19,7 +21,9 @@ import {FormsModule} from '@angular/forms';
     FooterAgentComponent,
     NavbarAgentComponent,
     SidebarAgentComponent,
-    AddClientComponent
+    AddClientComponent,
+    EditClientComponent,
+    DetailsClientComponent
   ],
   exports: [
     FooterAgentComponent,
---
+++ src/app/Agent/Agentcomponents/edit-client/edit-client.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {IClient} from "../../../models/Client";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ClientService} from "../../../service/client.service";
+
+@Component({
+  selector: 'app-edit-client',
+  templateUrl: './edit-client.component.html',
+  styleUrls: ['./edit-client.component.scss']
+})
+export class EditClientComponent implements OnInit {
+
+
+  public loading: boolean =false;
+  public id: string | null = null;
+  public client:IClient = {} as IClient;
+
+  constructor(private activatedRoute : ActivatedRoute, private clientService : ClientService, private router : Router){
+
+  }
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param)=>{
+      this.id =param.get('id');
+    });
+    if(this.id){
+      this.loading=true;
+      this.clientService.getClient(parseInt(this.id)).subscribe((data)=>{
+        this.client = data;
+        this.loading = false;
+      }, (error) => {
+        console.log(error);
+      })
+    }
+  }
+
+
+  public update(){
+    if(this.id){
+      this.clientService.updateClient(this.client, parseInt(this.id)).subscribe((data)=>{
+        this.router.navigate([`/admin`]).then();
+      }, (error) => {
+        this.router.navigate(["/edit-client/:id"]).then();
+      });
+    }
+
+  }
+
+}
