ccbd6cc24128db73c83e0b6ad67558f552878a74
Salwahnid <salwahnid1@gmail.com>
2024-05-24 17:56:34 +0100

    client.service

--- src/app/Agent/agent-dash/agent-dash.routing.ts
+++ src/app/Agent/agent-dash/agent-dash.routing.ts
@@ -2,12 +2,22 @@ import { Routes } from '@angular/router';
 import {DashboardAgentComponent} from '../pagesAgent/dashboardAgent/dashboardAgent.component';
 import {ProfileAgentComponent} from '../pagesAgent/profileAgent/profileAgent.component';
 import {AddClientComponent} from '../Agentcomponents/add-client/add-client.component';
+import {ProfileAdminComponent} from "../../Back-office/pages/profileAdmin/profileAdmin.component";
+import {NewAgentComponent} from "../../Back-office/pages/new-agent/new-agent.component";
+import {AddAgentComponent} from "../../Back-office/Admincomponents/add-agent/add-agent.component";
+import {EditAgentComponent} from "../../Back-office/Admincomponents/edit-agent/edit-agent.component";
+import {DetailsAgentComponent} from "../../Back-office/Admincomponents/details-agent/details-agent.component";
+import {DetailsClientComponent} from "../Agentcomponents/details-client/details-client.component";
+import {EditClientComponent} from "../Agentcomponents/edit-client/edit-client.component";
+import {ProfileClientComponent} from "../../client/clientsPages/profileClient/profileClient.component";
 
 
 
 
 export const AgentDashRoutes: Routes = [
   { path: 'agent',      component: DashboardAgentComponent },
-  { path: 'agent-profile',   component: ProfileAgentComponent },
+  { path: 'agent-profile',   component: ProfileClientComponent },
   {path: 'add-client',       component: AddClientComponent},
+  { path: 'edit-agent/:id', component: EditClientComponent },
+  { path: 'details-agent/:id', component: DetailsClientComponent }
 ];
--- src/app/Agent/pagesAgent/profileAgent/profileAgent.component.ts
+++ src/app/Agent/pagesAgent/profileAgent/profileAgent.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './profileAgent.component.html',
   standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   styleUrls: ['./profileAgent.component.scss']
 })
 export class ProfileAgentComponent implements OnInit {
